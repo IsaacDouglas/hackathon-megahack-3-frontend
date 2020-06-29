08a05217a8439b8fe72b44e8bdb5d960caeabaf3
Isaac Douglas <isaacdouglas2015@gmail.com>
2020-06-29 17:35:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Admin, Resource } from 'react-admin';
+import { UserList, UserEdit, UserCreate } from './Pages/user';
+import { GoalList, GoalEdit, GoalCreate } from './Pages/goal';
+
+import authProvider from './Provider/authProvider';
+import dataProvider from './Provider/dataProvider';
+
+import UserIcon from '@material-ui/icons/Group';
+
+import ptBrMessages from './language-pt-br';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+
+const i18nProvider = polyglotI18nProvider(() => ptBrMessages);
+
+const App = () => (
+    <Admin 
+        authProvider={authProvider} 
+        dataProvider={dataProvider} 
+        i18nProvider={i18nProvider}
+    >
+    <Resource 
+        icon={UserIcon} 
+        options={{ label: 'Usuários' }} 
+        name="user" 
+        list={UserList} 
+        edit={UserEdit} 
+        create={UserCreate} 
+    />
+    <Resource 
+        icon={UserIcon} 
+        options={{ label: 'Metas' }} 
+        name="goal" 
+        list={GoalList} 
+        edit={GoalEdit} 
+        create={GoalCreate} 
+    />
+    </Admin>
+);
 
 export default App;
---
+++ src/Pages/user.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useMediaQuery } from '@material-ui/core';
+import { List, SimpleList, Datagrid, TextField, EditButton, Edit, SimpleForm, TextInput, Create } from 'react-admin';
+import { required } from 'react-admin';
+
+export const UserList = ({permissions, ...props}) => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'))
+    
+    return (
+        <List
+        title="Usuários" 
+        {...props} >
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => `id: ${record.id}`}
+                    secondaryText={record => record.name}
+                    tertiaryText={record => record.username}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <TextField source="name" label="Nome"/>
+                    <TextField source="username" label="Username"/>
+                    <EditButton/>
+                </Datagrid>
+            )}
+        </List>
+    );
+}
+
+const UserTitle = ({ record }) => {
+    return <span>Nome: {record ? `"${record.name}"` : ''}</span>;
+};
+
+export const UserEdit = props => (
+    <Edit title={<UserTitle />} {...props}>
+        <SimpleForm >
+            <TextInput source="name" label="Nome" validate={required()}/>
+            <TextInput source="username" label="Username" validate={required()}/>
+        </SimpleForm>
+    </Edit>
+);
+
+export const UserCreate = props => (
+    <Create title= "Novo usuário" {...props}>
+        <SimpleForm>
+            <TextInput source="name" label="Nome" validate={required()}/>
+            <TextInput source="username" label="Username" validate={required()}/>
+        </SimpleForm>
+    </Create>
+);
